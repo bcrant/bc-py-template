9402123ea4208d06a3679173ba15a4673e698bd3
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-01 20:59:21 -0700

    Remove JSON log formatter.

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/_log.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/_log.py
@@ -1,9 +1,7 @@
-"""{{ cookiecutter.package_name }}/)_log.py"""
+"""{{ cookiecutter.package_name }}/_log.py"""
 
 import logging
 from typing import Dict, List
-
-import json_log_formatter
 
 
 LOG_FMT = (
@@ -16,8 +14,8 @@ class PathTruncatingFormatter(logging.Formatter):
 
     def format(self, record):
         """
-        # Logging Formatter subclass to truncate the pathname.
-        # This enables the log components to be evenly aligned.
+        Logging Formatter subclass to truncate the pathname.
+        This enables the log components to be evenly aligned.
         """
         if "pathname" in record.__dict__.keys():
             full_path = record.pathname
@@ -33,69 +31,10 @@ class PathTruncatingFormatter(logging.Formatter):
         return super(PathTruncatingFormatter, self).format(record)
 
 
-class TracingJSONLogFormatter(json_log_formatter.JSONFormatter):
-    """
-    Tracing JSON log formatter to convert log messages to JSON.
-    """
-
-    _log_builtin_attrs = (
-        "args",
-        "asctime",
-        "created",
-        "exc_info",
-        "exc_text",
-        "filename",
-        "funcName",  # 'levelname',
-        "levelno",  # 'lineno',
-        # 'module',
-        "msecs",
-        "message",
-        "msg",
-        "name",
-        "pathname",
-        "process",
-        "processName",
-        "relativeCreated",
-        "stack_info",
-        "thread",
-        "threadName",
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def extra_from_record(self, record: Dict) -> Dict:
-        """
-        Cleans up the log message using the provided self._log_builtin_attrs
-
-        :param record:
-        :return:
-        """
-        return {
-            attr_name: record.__dict__[attr_name]
-            for attr_name in record.__dict__
-            if attr_name not in self._log_builtin_attrs
-        }
-
-    def to_json(self, record: Dict) -> str:
-        """
-        Converts the log message to JSON.
-
-        :param record:
-        :return:
-        """
-        record["level"] = record["levelname"]
-        record["line_number"] = record["lineno"]
-        del record["levelname"]
-        del record["lineno"]
-        return self.json_lib.dumps(record, default=str)
-
-
 def setup_logging(
     app_name: str = None,
     modules: List[str] = [],
     debug: bool = True,
-    json_fmt: bool = True,
 ):
     """
     Sets up global logging for provided list of modules and the app_name provided
@@ -111,11 +50,9 @@ def setup_logging(
                             urllib3.connectionpool
                             uvicorn.access | uvicorn.error
         debug:          Defaults to True, can be disabled in Prod
-        json_fmt:       Defaults to True, applies TracingJsonLogFormatter
-                        False applies string PathTruncatingFormatter
 
     Returns:
-        logger object with JSON formatting.
+        logger instance.
     """
 
     level = logging.DEBUG if debug else logging.INFO
@@ -131,9 +68,6 @@ def setup_logging(
             logger.handlers.clear()
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(level)
-        if json_fmt:
-            stream_handler.setFormatter(TracingJSONLogFormatter())
-        else:
-            stream_handler.setFormatter(PathTruncatingFormatter(LOG_FMT))
+        stream_handler.setFormatter(PathTruncatingFormatter(LOG_FMT))
         logger.addHandler(stream_handler)
     return logging.getLogger(app_name)
